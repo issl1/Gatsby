714680bae5b9ef3489481f61c56acb22f96d28a8
issl1 <yinka@isslng.com>
2022-09-26 10:29:32 +0100

    Add files via upload

---
+++ api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../../node_modules/gatsby-plugin-web-font-loader/gatsby-browser.js'),
+      options: {"plugins":[],"google":{"families":["Poppins:400,600,700"]}},
+    },{
+      plugin: require('../../node_modules/gatsby/dist/internal-plugins/partytown/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
